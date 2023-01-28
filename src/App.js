c26313c646edc4ec5bf3c274da7a6b4af5697d33
Sitthisak Bannob <t-sitthisak.ban@axons.in.th>
2023-01-29 02:24:35 +0700

    main app component data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+
 function App() {
   return (
     <div className="App">
-      <p>test</p>
+      <React.Fragment>
+        <article>
+          <h1>บัญชีรายรับ - รายจ่าย</h1>
+          <p>บันทึกข้อมูลบัญชีในแต่ล่ะวัน</p>
+          <ul>
+            <li>ค่าเดินทาง <span>-200</span></li>
+            <li>เงินเดือน <span>+20,000</span></li>
+            <li>ค่าอาหาร <span>-500</span></li>
+          </ul>
+        </article>
+      </React.Fragment>
     </div>
   );
 }
